2fe8e7baac9f9c645bb3d2753db3500cc55f1961
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-07-27 09:55:14 +0200

    Turn off Spring banner & set logging level to warn

--- src/main/java/ua/dlc/callcentrecrm/CallCentreCrmApplication.java
+++ src/main/java/ua/dlc/callcentrecrm/CallCentreCrmApplication.java
@@ -1,13 +1,23 @@
 package ua.dlc.callcentrecrm;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CallCentreCrmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CallCentreCrmApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello world!");
+		};
 	}
 
 }
