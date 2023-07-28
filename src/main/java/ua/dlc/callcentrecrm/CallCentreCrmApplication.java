c77e0d845097a86e11b69823c09d78eb9b857ff9
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-07-28 18:09:48 +0200

    Move credentials to .env file

--- src/main/java/ua/dlc/callcentrecrm/CallCentreCrmApplication.java
+++ src/main/java/ua/dlc/callcentrecrm/CallCentreCrmApplication.java
@@ -2,10 +2,12 @@ package ua.dlc.callcentrecrm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@PropertySource("file:${user.dir}/.env")
 public class CallCentreCrmApplication {
 
     public static void main(String[] args) {
